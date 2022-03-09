138b3c8adcd3e94c69ec4a63b46b624d173d1718
ala <homazmora@gmail.com>
2022-03-09 14:56:49 +0100

    vtest

--- src/main/java/pr/ala/pizzaaplication/domain/model/OrderStatusType.java
+++ src/main/java/pr/ala/pizzaaplication/domain/model/OrderStatusType.java
@@ -1,5 +1,5 @@
 package pr.ala.pizzaaplication.domain.model;
 
 public enum OrderStatusType {
-    NEW, IN_PROGRESS, OUT_FOR_DELIVERY, DELIVERED, CANCELED, READY
+    NEW, IN_PROGRESS, OUT_FOR_DELIVERY, DELIVERED, CANCELED, READY, DONE
 }
